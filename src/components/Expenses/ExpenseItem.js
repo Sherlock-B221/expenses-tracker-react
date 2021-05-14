5f41433f243b0b87e04602b8088d3942670c5b61
Sherlock-B221 <mohanu526@gmail.com>
2021-05-14 19:43:55 +0530

    removed change title option

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,23 +4,11 @@ import {Card} from "../UI/Card";
 import React, { useState } from "react";
 
 export const ExpenseItem = (props) => {
-    const [title,setTitle] = useState(props.title);
-
-    const changeTitleHandler = () => {
-        if(title==='Updated!')
-        {
-            setTitle('henlo')
-        }
-        else
-
-        setTitle('Updated!')
-    }
     return <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <div className="expense-item__price">{props.price}</div>
         </div>
-        <button onClick={changeTitleHandler}>Change Title</button>
     </Card>
 }
