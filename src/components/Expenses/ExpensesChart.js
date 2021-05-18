7b67d140ec05088c24dd8b359e4e2dd34c53ae44
Sherlock-B221 <mohanu526@gmail.com>
2021-05-19 01:01:39 +0530

    fixed date issue, project complete

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -19,10 +19,12 @@ const ExpensesChart = (props) => {
     ];
 
     for (const expense of props.expenses) {
-        const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
+        console.log(props.expenses,'herjkha');
+        const date = new Date(expense.date)
+        const expenseMonth = date.getMonth(); // starting at 0 => January => 0
         chartDataPoints[expenseMonth].value += expense.amount;
     }
-
+//Todo: Check out what the problem is with the date for everywhere...
     return <Chart dataPoints={chartDataPoints} />;
 };
 
